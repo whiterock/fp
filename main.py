ae3b9e6c18f8d705b8bcd91205d539b613baedc2
whiterock <whiterockftp@gmail.com>
2020-09-08 14:16:22 +0200

    minor refactorings

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import string, sys
 from pprint import pprint
 
-
 parens = {
     "(": ")",
     "{": "}"
 }
 
-def partition(s, stop_on=None):
+
+def partition_rec(s, stop_on=None):
     result = []
     i = 0
     buffer = ""
@@ -16,7 +16,7 @@ def partition(s, stop_on=None):
         if cc == stop_on:
             if buffer:
                 result.append(buffer)
-            return result, i
+            return result, i + 1
         elif cc in parens:
             # empty previous buffer if non-empty
             if buffer:
@@ -24,8 +24,8 @@ def partition(s, stop_on=None):
                 buffer = ""
 
             # recursion here
-            a_partition, i_advance = partition(s[i+1:], stop_on=parens[cc])
-            i += i_advance + 2
+            a_partition, i_advance = partition_rec(s[i + 1:], stop_on=parens[cc])
+            i += i_advance + 1
             result.append(a_partition)
         elif cc == ",":
             if buffer:
@@ -42,90 +42,100 @@ def partition(s, stop_on=None):
     return result, i
 
 
+def partition(s):
+    part, length = partition_rec(s)
+    assert length == len(s)
+    return part
+
+
 def split_my_thing(l):
-    for n, i in enumerate(l):
-        if type(i) is list:
-            l[n] = split_my_thing(l[n])
-        elif type(i) is str and ' ' in i:
-            l = i.split()
+    for n, item in enumerate(l):
+        if type(item) is list:
+            l[n] = split_my_thing(item)
+        elif type(item) is str and ' ' in item:
+            l = item.split()
     return l
+
 
 environment = {
-	'plus': lambda a,b: a+b,
-	'minus': lambda a,b: a-b,
-	'mult': lambda a,b: a*b,
-	'div': lambda a,b: a/b,
-	'cond': lambda a,b,c: b if a else c
+    'plus': lambda a, b: a + b,
+    'minus': lambda a, b: a - b,
+    'mult': lambda a, b: a * b,
+    'div': lambda a, b: a / b,
+    'cond': lambda a, b, c: b if a else c
 }
 
-class lamb():
-	def __init__(self,variable_name,body,env):
-		self.variable_name = variable_name
-		self.body = body
-		self.env = env
 
-	def __str__(self):
-		return "Lambda: " + self.variable_name + "->" + str(self.body)
+class Lamb():
+    def __init__(self, variable_name, body, env):
+        self.variable_name = variable_name
+        self.body = body
+        self.env = env
 
-def recursive_replace(l, a,b):
+    def __str__(self):
+        return "Lambda: " + self.variable_name + "->" + str(self.body)
+
+
+def recursive_replace(l, a, b):
     for n, i in enumerate(l):
         if type(i) is list:
-            l[n] = recursive_replace(l[n], a,b)
+            l[n] = recursive_replace(l[n], a, b)
         elif type(i) is str:
-            l[n] = i.replace(a,b)
+            l[n] = i.replace(a, b)
     return l
 
+
 def evaluate(e, env=environment):
-	#print(e)
-	if isinstance(e[0], list):
-		a = evaluate(e[0], env)
-		e[0] = a
-		return evaluate(e, env)
-	elif isinstance(e[0], str):
-		try:
-			return int(e[0])
-		except:
-			pass
-		if "->" in e[0]:
-			variable_name = e[0][0:e[0].index('->')]
-			body = e[1]
+    # print(e)
+    if isinstance(e[0], list):
+        a = evaluate(e[0], env)
+        e[0] = a
+        return evaluate(e, env)
+    elif isinstance(e[0], str):
+        try:
+            return int(e[0])
+        except:
+            pass
+        if "->" in e[0]:
+            variable_name = e[0][0:e[0].index('->')]
+            body = e[1]
 
-			if len(e) == 3:
-				called_with = e[2]
-				new_body = recursive_replace(body, variable_name, called_with)
-				return evaluate(new_body, env)
+            if len(e) == 3:
+                called_with = e[2]
+                new_body = recursive_replace(body, variable_name, called_with)
+                return evaluate(new_body, env)
 
-			return lamb(variable_name, body, env)
-		elif "{" in e[0]:
-			pass #unimplemented
-		else:
-			#function call
-			name = e[0]
-			if name not in env.keys():
-				print("Error: unrecognised token " + name)
-				exit()
-			if name != "cond":
-				return env[name](evaluate(e[1]), evaluate(e[2]))
-			else:
-				return env[name](e[1],e[2],e[3])
-	elif isinstance(e[0], lamb):
-		#call lambda
-		try:
-			called_with = e[1]
-			new_body = recursive_replace(e[0].body, e[0].variable_name, called_with)
-			return evaluate(new_body, env)
-		except:
-			pass
+            return Lamb(variable_name, body, env)
+        elif "{" in e[0]:
+            pass  # unimplemented
+        else:
+            # function call
+            name = e[0]
+            if name not in env.keys():
+                print("Error: unrecognised token " + name)
+                exit()
+            if name != "cond":
+                return env[name](evaluate(e[1]), evaluate(e[2]))
+            else:
+                return env[name](e[1], e[2], e[3])
+    elif isinstance(e[0], Lamb):
+        # call lambda
+        try:
+            called_with = e[1]
+            new_body = recursive_replace(e[0].body, e[0].variable_name, called_with)
+            return evaluate(new_body, env)
+        except:
+            pass
 
 
 if __name__ == "__main__":
+    # if len(sys.argv) != 2:
+    # 	exit()
+    # p = sys.argv[1]
+    # test = "{a=5,b={c=3, d=4},e=7}((x->(y->+(* x x)y))2)3"
+    # pprint(test)
+    # pprint(partition(test)[0])
+    # print(partition("((x->(y->(plus(mult x x)y))2)3"))
 
-	# if len(sys.argv) != 2:
-	# 	exit()
-	# p = sys.argv[1]
-	# test = "{a=5,b={c=3, d=4},e=7}((x->(y->+(* x x)y))2)3"
-	# pprint(test)
-	# pprint(partition(test)[0])
-
-	print(evaluate(split_my_thing(partition("((x->(y->(plus(mult x x)y))2)3")[0][0])))
-	print(evaluate(split_my_thing(partition("(x->(y->(plus(mult x 2)(mult y y)))1)4)")[0])))
+    print(evaluate(split_my_thing(partition("((x->(y->(plus(mult x x)y))2)3")[0])))
+    print(evaluate(split_my_thing(partition("(x->(y->(plus(mult x 2)(mult y y)))1)4)"))))
