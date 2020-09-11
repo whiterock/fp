46708a4ffb0c2552e60836bced3f26b430f38904
whiterock <whiterockftp@gmail.com>
2020-09-11 21:04:31 +0200

    some clean up / refactoring and inspection work

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import string, sys, re, copy
 from pprint import pprint
 
 
-def next_char(e, i):
+def get_next_non_whitespace_char_index(e, i):
     while e[i] in string.whitespace:
         i += 1
     return i
@@ -11,32 +11,54 @@ def next_char(e, i):
 def next_closing_parenthesis(e, i):
     level = 0
     while i < len(e):
-        if e[i] == ')' and level == 0: return i
-        if e[i] == '(': level += 1
-        if e[i] == ')': level -= 1
-        i+=1
-    assert(1==2)
+        if e[i] == ')':
+            if level == 0:
+                return i
+            else:
+                level -= 1
+        elif e[i] == '(':
+            level += 1
+
+        i += 1
+    # should not be reached
+    raise SyntaxError("Unbalanced parenthesis at", i)
 
 
-def add_parentheses(e):
+def rescue_lambdas(s):
     k = 0
-    for i in [m.start()+2 for m in re.finditer('->', e)]:
-        if e[next_char(e, i+k)] != '(':
-            j = next_closing_parenthesis(e, i+k)
-            e = e[:i+k] + '(' + e[i+k:j] + ')' + e[j:]
+    for i in [m.start() + 2 for m in re.finditer('->', s)]:
+        if s[get_next_non_whitespace_char_index(s, i + k)] != '(':
+            j = next_closing_parenthesis(s, i + k)
+            s = s[:i + k] + '(' + s[i + k:j] + ')' + s[j:]
             k += 1
-    return e
+    return s
 
 
-def parse(e):
-    if '=' in e: e = e.replace(',', ',)').replace('=', '=(').replace('}', ')}')
-    e = add_parentheses(e)
-    if e[0] != '(' or e[-1] != ')': e = '(' + e + ')'
-    if '}' in e and '=' in e:       #TODO: turning { .. }minus(b 5)4 into { .. }(minus(b 5)4) might not be the best way to go about this.
-        i = e.rfind('}')+1
-        if i < len(e)-1: e = e[:i] + '(' + e[i:] + ')'
-    e = e.replace('(', ' ( ').replace(')', ' ) ').replace('{', ' { ').replace('}', ' } ').replace(',', ' ').replace('=', '= ').replace('\n', ' ').replace('\t', ' ').split()
-    return parse_tokens(e)
+def parse(s):
+    # fixme: herein lies an error
+    if '=' in s:
+        s = s.replace(',', ',)').replace('=', '=(').replace('}', ')}')
+    s = rescue_lambdas(s)
+    if s[0] != '(' or s[-1] != ')':
+        s = '(' + s + ')'
+
+    if '}' in s and '=' in s:  # TODO: turning { .. }minus(b 5)4 into { .. }(minus(b 5)4) might not be the best way to go about this.
+        i = s.rfind('}') + 1
+        if i < len(s) - 1:
+            s = s[:i] + '(' + s[i:] + ')'
+
+    ast = s.replace('(', ' ( '). \
+        replace(')', ' ) '). \
+        replace('{', ' { '). \
+        replace('}', ' } '). \
+        replace(',', ' '). \
+        replace('=', '= '). \
+        replace('\n', ' '). \
+        replace('\t', ' ').\
+        split()
+
+    print(ast)
+    return parse_tokens(ast)
 
 
 def parse_tokens(e):
@@ -54,8 +76,10 @@ def parse_tokens(e):
         e.pop(0)
         return r
     else:
-        try: return int(t)
-        except: return t
+        try:
+            return int(t)
+        except ValueError:
+            return t
 
 
 environment = {
@@ -63,7 +87,7 @@ environment = {
     'minus': lambda a, b: a - b,
     'mult': lambda a, b: a * b,
     'div': lambda a, b: a / b,
-    'cond': lambda a, b, c: b if a else c
+    'cond': lambda a, b, c: b if a else c  # q: not used (because lazy eval?)
 }
 
 
@@ -100,11 +124,11 @@ def evaluate(e, env=environment):
             return e[0:e.index('->')]
         elif e in env and e not in environment:
             return env[e]
-        return # Critical
+        return  # Critical, raise error? # this path was chosen with previously faulty "fac"
     if isinstance(e, list) and len(e) == 2:
-        try: #NOTE: this is how i set the environment rn :/
-            if '=' in e[0][0]: #is there a more general rule
-                new_env = evaluate(e[0], env) #this feels hacks
+        try:  # NOTE: this is how i set the environment rn :/
+            if '=' in e[0][0]:  # is there a more general rule
+                new_env = evaluate(e[0], env)  # this feels hacks
                 return evaluate(e[1], {**env, **new_env})
         except:
             pass
@@ -121,7 +145,7 @@ def evaluate(e, env=environment):
             return Lamb(variable_name, body, env)
         elif e[0] in environment:
             if len(e) == 3:
-                return environment[e[0]](evaluate(e[1], env), evaluate(e[2], env))              
+                return environment[e[0]](evaluate(e[1], env), evaluate(e[2], env))
             if e[0] == 'cond' and len(e) == 4:
                 c = evaluate(e[1], env)
                 if c == 0 or c == []:
@@ -132,7 +156,7 @@ def evaluate(e, env=environment):
             r = {}
             for i in range(0, len(e), 2):
                 name = e[i][:-1]
-                value = evaluate(e[i+1], {**env, **r})
+                value = evaluate(e[i + 1], {**env, **r})
                 r[name] = value
             return r
     if isinstance(e, list):
@@ -148,30 +172,28 @@ if __name__ == "__main__":
     # p = sys.argv[1]
     # test = "{a=5,b={c=3, d=4},e=7}((x->(y->+(* x x)y))2)3"
 
-    #check this out :)
+    # check this out :)
     # print(evaluate(parse("(x->y->plus(mult x x)y) 2 3")))
     # print(evaluate(parse("((x->(y->plus(mult x x)y))2)3")))
     # print(evaluate(parse("{a=x->y->plus(mult x x)y, b=a 2, c=b 3}")))
 
-    # assert(evaluate(parse("(x->y->plus(mult x x)y) 2 3")) == 7)
-    # assert(evaluate(parse("((x->(y->plus(mult x x)y))2)3")) == 7)
-    # assert(evaluate(parse("minus ((y->plus(mult 2 2)y)5)7")) == 2)
-    # assert(evaluate(parse("{a=x->y->plus(mult x x)y, b=a 2, c=b 3}minus(b 5)c")) == 2)
-    # assert(evaluate(parse("cond 14 2 3")) == 2)
-    # assert(evaluate(parse("cond 0 2 3")) == 3)
-    # assert(evaluate(parse("cond {} 2 3")) == 3)
+    assert(evaluate(parse("(x->y->plus(mult x x)y) 2 3")) == 7)
+    assert(evaluate(parse("((x->(y->plus(mult x x)y))2)3")) == 7)
+    assert(evaluate(parse("minus ((y->plus(mult 2 2)y)5)7")) == 2)
+    assert(evaluate(parse("{a=x->y->plus(mult x x)y, b=a 2, c=b 3}minus(b 5)c")) == 2)
+    assert(evaluate(parse("cond 14 2 3")) == 2)
+    assert(evaluate(parse("cond 0 2 3")) == 3)
+    assert(evaluate(parse("cond {} 2 3")) == 3)
+    assert evaluate(parse("{fac=x->(cond x (mult x(fac (minus x 1))) 1)} fac 10")) == 3628800
 
-    # print(evaluate(parse("""{
-    #     append = x->y->cond x {head=x head, tail=append(x tail)y} y,
-    #     gen = x->cond x (append(gen(minus x 1)) {head=x, tail={}}) {}
-    #     }
-    #     gen 3""")))
+    print(evaluate(parse("""{
+        append= x->y->cond x {head=x head, tail=append(x tail)y} y,
+        gen=x->cond x (append(gen(minus x 1)) {head=x, tail={}}) {}
+        }
+        gen 3""")))
 
-
-    print(evaluate(parse("{fac=x->(cond x (mult x(fac (minus x 1))) 1)} fac 10")))
-
-    #print()
-    #debug("{fac=x->(cond x (mult x(fac (minus x 1))) 1)} fac 4")
+    # print()
+    # debug("{fac=x->(cond x (mult x(fac (minus x 1))) 1)} fac 4")
 
     # print(evaluate(parse("""   
     # (x->
@@ -182,6 +204,3 @@ if __name__ == "__main__":
     #     )1
     # )4
     # """)))
-
-
-
