73a782d525158335dae117ca4a51f42b850f3d07
whiterock <whiterockftp@gmail.com>
2020-09-11 18:10:15 +0200

    fixed env passing so recursion works
    this was some painful debugging :P

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import string, sys, re, copy
 from pprint import pprint
 
+
 def next_char(e, i):
     while e[i] in string.whitespace:
         i += 1
     return i
+
 
 def next_closing_parenthesis(e, i):
     level = 0
@@ -15,14 +17,16 @@ def next_closing_parenthesis(e, i):
         i+=1
     assert(1==2)
 
+
 def add_parentheses(e):
     k = 0
     for i in [m.start()+2 for m in re.finditer('->', e)]:
-        if e[next_char(e,i+k)] != '(':
-            j = next_closing_parenthesis(e,i+k)
+        if e[next_char(e, i+k)] != '(':
+            j = next_closing_parenthesis(e, i+k)
             e = e[:i+k] + '(' + e[i+k:j] + ')' + e[j:]
-            k+=1
+            k += 1
     return e
+
 
 def parse(e):
     if '=' in e: e = e.replace(',', ',)').replace('=', '=(').replace('}', ')}')
@@ -33,6 +37,7 @@ def parse(e):
         if i < len(e)-1: e = e[:i] + '(' + e[i:] + ')'
     e = e.replace('(', ' ( ').replace(')', ' ) ').replace('{', ' { ').replace('}', ' } ').replace(',', ' ').replace('=', '= ').replace('\n', ' ').replace('\t', ' ').split()
     return parse_tokens(e)
+
 
 def parse_tokens(e):
     t = e.pop(0)
@@ -51,6 +56,7 @@ def parse_tokens(e):
     else:
         try: return int(t)
         except: return t
+
 
 environment = {
     'plus': lambda a, b: a + b,
@@ -94,7 +100,7 @@ def evaluate(e, env=environment):
             return e[0:e.index('->')]
         elif e in env and e not in environment:
             return env[e]
-        return
+        return # Critical
     if isinstance(e, list) and len(e) == 2:
         try: #NOTE: this is how i set the environment rn :/
             if '=' in e[0][0]: #is there a more general rule
@@ -105,7 +111,7 @@ def evaluate(e, env=environment):
     if isinstance(e, int) or isinstance(e, Lamb) or isinstance(e, dict):
         return e
     if isinstance(e[0], Lamb) and isinstance(e[1], int):
-        body_copy = copy.deepcopy(e[0].body)
+        body_copy = copy.deepcopy(e[0].body)  # what for? strings are immutable
         new_body = recursive_replace(body_copy, e[0].variable_name, e[1])
         return evaluate([evaluate(new_body, env)] + e[2:], env)
     if isinstance(e[0], str):
@@ -117,12 +123,12 @@ def evaluate(e, env=environment):
             if len(e) == 3:
                 return environment[e[0]](evaluate(e[1], env), evaluate(e[2], env))              
             if e[0] == 'cond' and len(e) == 4:
-                c = evaluate(e[1])
+                c = evaluate(e[1], env)
                 if c == 0 or c == []:
-                    return evaluate(e[3])
-                return evaluate(e[2])
+                    return evaluate(e[3], env)
+                return evaluate(e[2], env)
         elif '=' in e[0]:
-            assert len(e)%2 == 0
+            assert len(e) % 2 == 0
             r = {}
             for i in range(0, len(e), 2):
                 name = e[i][:-1]
@@ -133,8 +139,7 @@ def evaluate(e, env=environment):
         a = []
         for b in e:
             a.append(evaluate(b, env))
-        return evaluate(a)
-
+        return evaluate(a, env)
 
 
 if __name__ == "__main__":
@@ -148,13 +153,13 @@ if __name__ == "__main__":
     # print(evaluate(parse("((x->(y->plus(mult x x)y))2)3")))
     # print(evaluate(parse("{a=x->y->plus(mult x x)y, b=a 2, c=b 3}")))
 
-    assert(evaluate(parse("(x->y->plus(mult x x)y) 2 3")) == 7)
-    assert(evaluate(parse("((x->(y->plus(mult x x)y))2)3")) == 7)
-    assert(evaluate(parse("minus ((y->plus(mult 2 2)y)5)7")) == 2)
-    assert(evaluate(parse("{a=x->y->plus(mult x x)y, b=a 2, c=b 3}minus(b 5)c")) == 2)
-    assert(evaluate(parse("cond 14 2 3")) == 2)
-    assert(evaluate(parse("cond 0 2 3")) == 3)
-    assert(evaluate(parse("cond {} 2 3")) == 3)
+    # assert(evaluate(parse("(x->y->plus(mult x x)y) 2 3")) == 7)
+    # assert(evaluate(parse("((x->(y->plus(mult x x)y))2)3")) == 7)
+    # assert(evaluate(parse("minus ((y->plus(mult 2 2)y)5)7")) == 2)
+    # assert(evaluate(parse("{a=x->y->plus(mult x x)y, b=a 2, c=b 3}minus(b 5)c")) == 2)
+    # assert(evaluate(parse("cond 14 2 3")) == 2)
+    # assert(evaluate(parse("cond 0 2 3")) == 3)
+    # assert(evaluate(parse("cond {} 2 3")) == 3)
 
     # print(evaluate(parse("""{
     #     append = x->y->cond x {head=x head, tail=append(x tail)y} y,
@@ -162,7 +167,8 @@ if __name__ == "__main__":
     #     }
     #     gen 3""")))
 
-    #print(evaluate(parse("{fac=x->(cond x (mult x(fac (minus x 1))) 1)} fac 4")))
+
+    print(evaluate(parse("{fac=x->(cond x (mult x(fac (minus x 1))) 1)} fac 10")))
 
     #print()
     #debug("{fac=x->(cond x (mult x(fac (minus x 1))) 1)} fac 4")
